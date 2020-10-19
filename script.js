c86c3347b757f897daccd9fb42b2eec217b3f343
AKGriffin92 <22640750+AKGriffin92@users.noreply.github.com>
2020-10-18 23:27:07 -0400

    add draw text function

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ drawRect( 0 , 0 , cvs.width , cvs.height , "BLACK" )
 function drawCircle( x , y , r , color ){
   ctx.fillStyle = color ;
   ctx.beginPath() ;
-  ctx.arc( x , y , r , 0 , Math.PI * 2 , false)
+  ctx.arc( x , y , r , 0 , Math.PI * 2 , false) ;
   ctx.closePath() ;
   ctx.fill() ;
 } ;
 
 drawCircle( 100 , 100 , 50 , "WHITE")
+
+// draw text
+function drawText( text , x , y , color){
+  ctx.fillStyle = color ;
+  ctx.font = "45px fantasy" ;
+  ctx.fillText( text , x , y) ;
+} ;
+
+drawText( "This is test text." , 300 , 200 )
